08825af4c74fd1633a60ce26161a4093af588b93
Eduardo Alvarado <ealvara73@gmail.com>
2023-06-29 13:39:21 -0500

    Add additional functions to LinkedList class

--- linked-list.js
+++ linked-list.js
@@ -1,14 +1,14 @@
 class LinkedList {
   constructor() {
-    this.head = null;
+    this._head = null;
   }
 
   append(value) {
     const newNode = new Node(value);
-    if (this.head === null) {
+    if (this._head === null) {
       this.head = newNode;
     } else {
-      let nextNode = this.head;
+      let nextNode = this._head;
       while (nextNode.nextNode) {
         nextNode = nextNode.nextNode;
       }
@@ -17,11 +17,11 @@ class LinkedList {
   }
 
   prepend(value) {
-    if (this.head === null) {
+    if (this._head === null) {
       const newNode = new Node(value);
       this.head = newNode;
     } else {
-      const newNode = new Node(value, this.head);
+      const newNode = new Node(value, this._head);
       this.head = newNode;
     }
   }
@@ -32,15 +32,92 @@ class LinkedList {
 
   calcSize() {
     let counter = 1;
-    if (this.head === null) return 0;
+    if (this._head === null) return 0;
     else {
-      let node = this.head;
+      let node = this._head;
       while(node.nextNode) {
         counter += 1;
         node = node.nextNode;
       }
       return counter;
     }
+  }
+
+  get head() {
+    return this._head;
+  }
+
+  set head(value) {
+    this._head = value;
+  }
+
+  get tail() {
+    return this.calcTail();
+  }
+
+  calcTail() {
+    if (this._head === null) return null;
+    else {
+      let node = this._head;
+      while (node.nextNode) node = node.nextNode;
+      return node;
+    }
+  }
+
+  at(index) {
+    let node = this._head;
+    for (let i = 0; i < index; i += 1) {
+      if(node === null) return null;
+      node = node.nextNode;
+    }
+    return node;
+  }
+
+  pop() {
+    if (this._head === null) return null;
+    if (this._head.nextNode === null) this._head = null;
+    else {
+      let node = this._head;
+      for (let i = 0; i < this.calcSize() - 2; i += 1) {
+        node = node.nextNode;
+      }
+      node.nextNode = null;
+    }
+  }
+
+  contains(value) {
+    if (this._head === null) return false;
+    let node = this._head;
+    for (let i = 0; i < this.calcSize(); i += 1) {
+      if (node.value === value) return true;
+      node = node.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    if (this._head === null) return null;
+    let node = this._head;
+    for (let i = 0; i < this.calcSize(); i += 1) {
+      if (node.value === value) return i;
+      node = node.nextNode;
+    }
+    return null
+  }
+
+  toString () {
+    let linkedListString = '';
+    if (this._head === null) return null;
+    let node =  this._head;
+    for (let i = 0; i < this.calcSize(); i += 1) {
+      if (node.nextNode === null) {
+        linkedListString += `${node.value} -> Null`;
+      } else {
+        linkedListString += `${node.value} -> `;
+        node = node.nextNode;
+      }
+    }
+    return linkedListString;
   }
 }
 
@@ -53,7 +130,7 @@ class Node {
 
 const newList = new LinkedList();
 newList.append(6)
+newList.append(7);
 newList.append(8);
-newList.append(8);
-newList.append(8);
-console.log(newList.size);
+newList.append(9);
+console.log(newList.toString());
